936450a350ae51d4b20bb2f807eec836fe308761
Otatopot <rochelewebs24@gmail.com>
2025-11-23 23:14:57 -0500

    Modified Landing Page Layout and implemented Javascript function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", () => {
+    var email = '';
+    var subscribe = document.getElementsByTagName('button')[0];
+    var message = document.getElementsByClassName("message")[0];
+    
+    subscribe.onclick = getmail;
+    
+    
+    function getmail(event){
+        event.preventDefault();
+        email = document.getElementById('email').value;
+        if (email == "") {
+            message.innerHTML = "Please enter a valid email address";
+        }
+        else {
+            message.innerHTML = "Thank you! Your email address " + email + " has been added to our email list."
+        }
+
+    }
+
+})
